d81b1a19b8a8e7bdd24a4ff38a4b908ab0ad9cc7
DRMagargee <45697532+legosmith311@users.noreply.github.com>
2019-09-27 13:14:43 -0400

    Added script for AI pathing and player detection

---
+++ StealthGame/Assets/scripts/failDetect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class failDetect : MonoBehaviour
+{
+
+    public Transform respawn;
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Detector"))
+        {
+            gameObject.transform.position = new Vector3 (0.0f, 1.0f, 0.0f);
+        }
+    }
+}

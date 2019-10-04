568a0dab81608b4696cd264feb6ffb5f0a5bc40f
DRMagargee <45697532+legosmith311@users.noreply.github.com>
2019-10-03 20:55:52 -0400

    Working Prototype 1

---
+++ StealthGame/Assets/scripts/enemyPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyPath : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+
+    private int current = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (transform.position.y != 1.049093)
+        //    gameObject.transform.position = new Vector3(transform.position.x, 1.049093f, transform.position.y);
+
+
+        if (transform.position.x != target[current].position.x || transform.position.z != target[current].position.z)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else
+        {
+            
+            Vector3 eulerRotation = new Vector3(transform.eulerAngles.x, target[current].transform.eulerAngles.y, transform.eulerAngles.z);
+            transform.rotation = Quaternion.Euler(eulerRotation);
+            current = (current + 1) % target.Length;
+            //transform.Rotate(0, -90, 0);
+        }
+    }
+}

23b8c1f576872a0ee283ecf6819fd2d18d536329
DRMagargee <45697532+legosmith311@users.noreply.github.com>
2019-10-12 17:15:23 -0400

    Game Jam 10.12 Build

--- StealthGame/Assets/scripts/stunEnemy.cs
+++ StealthGame/Assets/scripts/stunEnemy.cs
@@ -7,20 +7,23 @@ public class stunEnemy : MonoBehaviour
 
     public float cooldown = 00.0f;
     float cooldownRemaining = 0;
+    float cooldownRemaining2 = 0;
     public float range = 100.0f;
+    public float melee = 1f;
     public float duration = 10f;
     public float damage = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         cooldownRemaining -= Time.deltaTime;
+        cooldownRemaining2 -= Time.deltaTime;
 
         if (Input.GetMouseButton(0) && cooldownRemaining <= 0)
         {
@@ -43,9 +46,63 @@ public class stunEnemy : MonoBehaviour
                     t.hit();
                 }
 
-                
+
 
             }
         }
+        /*
+        if (Input.GetKeyDown("q") && cooldownRemaining2 <= 0)
+        {
+            cooldownRemaining2 = cooldown;
+            Debug.Log("Spaaaaace");
+
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hitInfo;
+
+
+            if(hit.collider.gameObject == A1L1)
+            {
+
+            }
+            if (Physics.Raycast(ray, out hitInfo, melee))
+            {
+                Vector3 hitPoint = hitInfo.point;
+                GameObject go = hitInfo.collider.gameObject;
+                Debug.Log("Hit Object: " + go.name);
+                Debug.Log("Hit Point: " + hitPoint);
+
+                takesDamage t = go.GetComponent<takesDamage>();
+
+                if (t != null)
+                {
+                    t.hit();
+                }
+
+
+
+            }
+        }
+        */
     }
+    /*
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        //Debug.Log("Target Aquired");
+        if (Input.GetKeyDown("q"))
+        {
+            Debug.Log("Target Aquired");
+            if (collisionInfo.gameObject.tag == "Dummy")
+            {
+                Debug.Log("BONK");
+                GameObject go = collisionInfo.collider.gameObject;
+                takesDamage t = go.GetComponent<takesDamage>();
+
+                if (t != null)
+                {
+                    t.hit();
+                }
+            }
+        }
+    }
+    */
 }
--- StealthGame/Assets/scripts/takesDamage.cs
+++ StealthGame/Assets/scripts/takesDamage.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class takesDamage : MonoBehaviour
 {
+
+    public static takesDamage instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void hit()
     {
         failDetect.instance.up = 300;
-        
     }
 
     /*public void RecieveDamage(float amount)
